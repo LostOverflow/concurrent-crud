d13390c2ae884f0bdea8c06f28129375b202cad6
TuzPavel <TuzPavel@gmail.com>
2024-04-23 18:09:45 +0200

    Huh... Here we are! Let's do the test task for sport radar! I need some coffee and a rest ;)

---
+++ src/main/java/sportradar/demo/simplebet/SimpleBetApplication.java
@@ -0,0 +1,13 @@
+package sportradar.demo.simplebet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sportradar/demo/simplebet/SimpleBetApplicationTests.java
@@ -0,0 +1,13 @@
+package sportradar.demo.simplebet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
