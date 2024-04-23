6be68bccab4af92da7bdca571fd48336fb8d1cd8
TuzPavel <TuzPavel@gmail.com>
2024-04-23 22:29:44 +0200

    Always use code formatter (preferred by the team) to avoid non-valuable changes in the code. Cases for update added

--- README.md
+++ README.md
@@ -6,8 +6,12 @@
 
 # Notes:
 
-Tech requirements a little inconsistent: nothing was mentioned about 'thread safety' in PDF test file but was
+Tech requirements a little inconsistent:
+
+1. nothing was mentioned about 'thread safety' in PDF test file but was
 mentioned at the notification email. If to keep following all recommendations very strictly you could become to a
 question: should I implement it in a thread safety style? If yes, then I probably could break KISS rule.
+2. Get summary API method is actually not described about returning result. Not clear of should we just print result or
+   return an object
 
 During development keep always spell checker on, and check correct names. Apply into dictionary if it needs.
--- src/test/java/sportradar/demo/football/FootballScoreboardApplicationTests.java
+++ src/test/java/sportradar/demo/football/FootballScoreboardApplicationTests.java
@@ -42,7 +42,6 @@ import org.springframework.boot.test.context.SpringBootTest;
  * desc: trying to add a new match when AWAY team is already playing in OTHER match
  * verify: validation exception has thrown: TeamAlreadyPlayingException
  * <p>
- *
  * API:
  * signature: updateScore(int homeTeamScore, int awayTeamScore)
  * desc: Receives a pair of absolute scores: home team score and away team score
@@ -71,8 +70,53 @@ import org.springframework.boot.test.context.SpringBootTest;
  * invoke: update match B
  * verify: match B updated with the new score
  * <p>
+ *     Note:
  * I have spent some time thinking on how to validate Update scores. But some lack of football rules:
- * could be the scores updated more than +/-1 at ones? Could it be reset to 0/0 if any emergency happened?
+ * could be the scores updated more than +/-1 at one time? Could it be reset to 0/0 if any emergency happened?
+ * Decided not to apply any validation for update yet
+ * <p>
+ *  API:
+ *  signature: finishMatch(String homeTeam, String awayTeam)
+ *  desc: Receives a pair of home and away teams and Finishes match currently in progress.
+ *  Removes match from the scoreboard.
+ * <p>
+ * Test cases:
+ *
+ *  Note: "removing from" operation could be implemented in both ways:
+ *      * throw an exception if search item was not there
+ *      * ignore all if search item was not there
+ *      If we want to keep idempotency during repeated operations - then should ignore
+ *
+ *     I prefer to throw an exception, that is more informative
+ * <p>
+ * name: 'Remove last match'
+ * desc: Removes last match from dashboard
+ * make sure: board is empty (getMatches should return empty list)
+ * invoke: start any new match and verify it
+ * invoke: finishMatch which was created before
+ * verify: board is empty (getMatches should return empty list)
+ * <p>
+ * name: 'Remove From empty board'
+ * desc: Tries to remove match from empty board
+ * make sure: exception has throws: MatchNotStartedException(String homeTeam, String awayTeam)
+ * <p>
+ * TODO could add double removing the same match test case
+ * <p>
+ * name: 'Remove From multi match board'
+ * desc: Removes dedicated match from multi match board
+ * make sure: board is empty (getMatches should return empty list)
+ * invoke: start match A and verify it
+ * invoke: start match B and verify it
+ * invoke: remove match A, verify match B is only on the board
+ * <p>
+ *
+ * <p>
+ *  API:
+ *  signature: getMatches(String homeTeam, String awayTeam)
+ *  desc: Receives a pair of home and away teams and Finishes match currently in progress.
+ *  Removes match from the scoreboard.
+ * <p>
+ *
  */
 @SpringBootTest
 class FootballScoreboardApplicationTests {
